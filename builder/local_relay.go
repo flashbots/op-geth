4d085c4f5a5b863199161a7d72a90abdde5d743a
Jinsuk Park <totorovirus@gmail.com>
2024-08-16 19:34:15 +0900

    remove bestHeader and use VersionedSubmitBlockRequest method to get slot and parent hash

--- builder/local_relay.go
+++ builder/local_relay.go
@@ -1,7 +1,6 @@
 package builder
 
 import (
-	"bytes"
 	"encoding/json"
 	"errors"
 	"fmt"
@@ -12,11 +11,7 @@ import (
 
 	builderSpec "github.com/attestantio/go-builder-client/spec"
 	consensusspec "github.com/attestantio/go-eth2-client/spec"
-	"github.com/attestantio/go-eth2-client/spec/bellatrix"
-	"github.com/attestantio/go-eth2-client/spec/deneb"
 	"github.com/attestantio/go-eth2-client/spec/phase0"
-	eth2UtilBellatrix "github.com/attestantio/go-eth2-client/util/bellatrix"
-	eth2UtilCapella "github.com/attestantio/go-eth2-client/util/capella"
 	"github.com/ethereum/go-ethereum/common/hexutil"
 	"github.com/ethereum/go-ethereum/log"
 	"github.com/flashbots/go-boost-utils/bls"
@@ -52,9 +47,8 @@ type LocalRelay struct {
 
 	enableBeaconChecks bool
 
-	bestDataLock   sync.Mutex
-	bestHeader     *deneb.ExecutionPayloadHeader
-	bestSubmission *builderSpec.VersionedSubmitBlockRequest
+	bestSubmissionLock sync.Mutex
+	bestSubmission     *builderSpec.VersionedSubmitBlockRequest
 
 	indexTemplate *template.Template
 	fd            ForkData
@@ -110,16 +104,10 @@ func (r *LocalRelay) SubmitBlock(msg *builderSpec.VersionedSubmitBlockRequest, _
 	}
 
 	log.Info("submitting block to local relay", "msg", msg, "version", msg.Version)
-	header, err := PayloadToPayloadHeader(msg.Deneb.ExecutionPayload)
-	if err != nil {
-		log.Error("could not convert payload to header", "err", err)
-		return err
-	}
 
-	r.bestDataLock.Lock()
+	r.bestSubmissionLock.Lock()
 	r.bestSubmission = msg
-	r.bestHeader = header
-	r.bestDataLock.Unlock()
+	r.bestSubmissionLock.Unlock()
 
 	return nil
 }
@@ -159,28 +147,41 @@ func (r *LocalRelay) handleGetPayloadTrusted(w http.ResponseWriter, req *http.Re
 	}
 	parentHashHex := vars["parent_hash"]
 
-	r.bestDataLock.Lock()
-	bestHeader := r.bestHeader
+	r.bestSubmissionLock.Lock()
 	bestSubmission := r.bestSubmission
-	r.bestDataLock.Unlock()
+	r.bestSubmissionLock.Unlock()
 
-	log.Info("Received unblinded(trusted) block request", "bestHeader", bestHeader, "bestSubmission", bestSubmission)
+	log.Info("Received unblinded(trusted) block request", "bestSubmission", bestSubmission)
 
-	if bestHeader == nil || bestSubmission == nil {
+	if bestSubmission == nil {
 		log.Error("no builder submissions")
 		respondError(w, http.StatusInternalServerError, "no payloads")
 		return
 	}
 
-	if bestHeader.BlockNumber != uint64(slot) {
-		log.Error("slot not equal", "requested", slot, "best", bestHeader.BlockNumber)
-		respondError(w, http.StatusBadRequest, fmt.Sprintf("slot not equal requested: %d bestPayload: %d", slot, bestHeader.BlockNumber))
+	submittedSlot, err := bestSubmission.Slot()
+	if err != nil {
+		log.Error("could not get slot from best submission", "err", err)
+		respondError(w, http.StatusInternalServerError, "internal server error")
 		return
 	}
 
-	if bestHeader.ParentHash.String() != parentHashHex {
-		log.Error("parent hash not equal", "requested", parentHashHex, "best", bestHeader.ParentHash.String())
-		respondError(w, http.StatusBadRequest, fmt.Sprintf("parent hash not equal requested: %s bestPayload: %s", parentHashHex, bestHeader.ParentHash.String()))
+	if submittedSlot != uint64(slot) {
+		log.Error("slot not equal", "requested", slot, "best", submittedSlot)
+		respondError(w, http.StatusBadRequest, fmt.Sprintf("slot not equal requested: %d bestPayload: %d", slot, submittedSlot))
+		return
+	}
+
+	submittedParentHash, err := bestSubmission.ParentHash()
+	if err != nil {
+		log.Error("could not get parent hash from best submission", "err", err)
+		respondError(w, http.StatusInternalServerError, "internal server error")
+		return
+	}
+
+	if submittedParentHash.String() != parentHashHex {
+		log.Error("parent hash not equal", "requested", parentHashHex, "best", submittedParentHash.String())
+		respondError(w, http.StatusBadRequest, fmt.Sprintf("parent hash not equal requested: %s bestPayload: %s", parentHashHex, submittedParentHash.String()))
 		return
 	}
 
@@ -214,50 +215,4 @@ func respondError(w http.ResponseWriter, code int, message string) {
 
 func (r *LocalRelay) handleStatus(w http.ResponseWriter, req *http.Request) {
 	w.WriteHeader(http.StatusOK)
-}
-
-func ExecutionPayloadHeaderEqual(l, r *deneb.ExecutionPayloadHeader) bool {
-	return l.ParentHash == r.ParentHash && l.FeeRecipient == r.FeeRecipient && l.StateRoot == r.StateRoot && l.ReceiptsRoot == r.ReceiptsRoot && l.LogsBloom == r.LogsBloom && l.PrevRandao == r.PrevRandao && l.BlockNumber == r.BlockNumber && l.GasLimit == r.GasLimit && l.GasUsed == r.GasUsed && l.Timestamp == r.Timestamp && l.BaseFeePerGas == r.BaseFeePerGas && bytes.Equal(l.ExtraData, r.ExtraData) && l.BlockHash == r.BlockHash && l.TransactionsRoot == r.TransactionsRoot && l.WithdrawalsRoot == r.WithdrawalsRoot && l.BlobGasUsed == r.BlobGasUsed && l.ExcessBlobGas == r.ExcessBlobGas
-}
-
-// PayloadToPayloadHeader converts an ExecutionPayload to ExecutionPayloadHeader
-func PayloadToPayloadHeader(p *deneb.ExecutionPayload) (*deneb.ExecutionPayloadHeader, error) {
-	if p == nil {
-		return nil, errors.New("nil payload")
-	}
-
-	var txs []bellatrix.Transaction
-	txs = append(txs, p.Transactions...)
-
-	transactions := eth2UtilBellatrix.ExecutionPayloadTransactions{Transactions: txs}
-	txroot, err := transactions.HashTreeRoot()
-	if err != nil {
-		return nil, err
-	}
-
-	withdrawals := eth2UtilCapella.ExecutionPayloadWithdrawals{Withdrawals: p.Withdrawals}
-	wdr, err := withdrawals.HashTreeRoot()
-	if err != nil {
-		return nil, err
-	}
-
-	return &deneb.ExecutionPayloadHeader{
-		ParentHash:       p.ParentHash,
-		FeeRecipient:     p.FeeRecipient,
-		StateRoot:        p.StateRoot,
-		ReceiptsRoot:     p.ReceiptsRoot,
-		LogsBloom:        p.LogsBloom,
-		PrevRandao:       p.PrevRandao,
-		BlockNumber:      p.BlockNumber,
-		GasLimit:         p.GasLimit,
-		GasUsed:          p.GasUsed,
-		Timestamp:        p.Timestamp,
-		ExtraData:        p.ExtraData,
-		BaseFeePerGas:    p.BaseFeePerGas,
-		BlockHash:        p.BlockHash,
-		TransactionsRoot: txroot,
-		WithdrawalsRoot:  wdr,
-		BlobGasUsed:      p.BlobGasUsed,
-		ExcessBlobGas:    p.ExcessBlobGas,
-	}, nil
 }
